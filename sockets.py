7c17d706808b3f87f863405cb73c274dec074308
Tyler <tlazaren@ualberta.ca>
2019-03-22 12:36:16 -0600

    I think done, front end changes and some back end webservice added

--- sockets.py
+++ sockets.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 import flask
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, jsonify
 from flask_sockets import Sockets
 import gevent
 from gevent import queue
@@ -127,23 +127,30 @@ def flask_post_json():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
 	'''update the entities via this interface'''
-	return None
+	myWorld.set(entity, flask_post_json())
+	return jsonify(myWorld.get(entity))
+
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
 	'''you should probably return the world here'''
-	return None
+	print("got request for world")
+	print(myWorld.world())
+	return jsonify(myWorld.world())
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
 	'''This is the GET version of the entity interface, return a representation of the entity'''
-	return None
+	return jsonify(myWorld.get(entity))
+
 
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
 	'''Clear the world out!'''
-	return None
+	myWorld.clear()
+	return jsonify({})
+
 
 
 
